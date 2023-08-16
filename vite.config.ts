68474ea7603a625177e9c476ea98f12e8bb95db1
王昌 <1255272899@qq.com>
2023-08-16 16:52:04 +0800

    eslint: 初始化eslnt

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server:{
-    port: 3000,
-    open: true,
-    host: 'localhost'
-  }
+  // server:{
+  //   port: 3000,
+  //   open: true,
+  //   host: 'localhost'
+  // }
 })
