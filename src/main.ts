fe2aaa14c5f756d30291c3a6e2444e2dacdac630
王昌 <1255272899@qq.com>
2023-08-17 14:31:59 +0800

    feat: svg插件

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import 'element-plus/dist/index.css'
 // 国际化
 import zhCn from 'element-plus/dist/locale/zh-tw.mjs'
 // import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// svg插件需要配置代码
+import 'virtual:svg-icons-register'
 import App from '@/App.vue'
 
 const app = createApp(App)
@@ -11,4 +13,5 @@ const app = createApp(App)
 app.use(ElementPlus, {
   locale: zhCn,
 })
+console.log(import.meta.env)
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-
+// 引入svg
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   base: './',
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+      symbolId: 'icon-[dir]-[name]',
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
