41bca13e3bcfb4c26a8caf145ed882ed7ead57dd
王昌 <1255272899@qq.com>
2024-01-03 09:41:44 +0800

    perf: 添加自动导入依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { ConfigEnv, UserConfigExport, loadEnv } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { viteMockServe } from 'vite-plugin-mock'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
-import { viteMockServe } from 'vite-plugin-mock'
+
 import { resolve } from 'path'
 // 引入svg
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
@@ -21,6 +23,10 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
       }),
       viteMockServe({
         localEnabled: command === 'serve', // 保证开发阶段可以用mock
+      }),
+      AutoImport({
+        // 可以自动全局引入全局依赖，减少重复引入 ref ,reactive,等
+        imports: ['vue', 'vue-router'],
       }),
     ],
     resolve: {
