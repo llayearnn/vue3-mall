7bf2225ea1f32212276d9d5216aa2a16cebf3da7
王昌 <1255272899@qq.com>
2024-06-07 10:08:57 +0800

    init: 代码提交约束

---
+++ scripts/commit.mjs
@@ -0,0 +1,23 @@
+import pico from 'picocolors'
+import { readFileSync } from 'fs'
+const msgPath = process.env.HUSKY_GIT_PARAMS
+const msg = readFileSync(msgPath, 'utf-8').trim()
+
+const commitRE =
+  /^(revert: )?(feat|init|merge|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)?: .{1,50}/
+const commitRE1 =
+  /^(revert: )?(feat|init|merge|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg) && !commitRE1.test(msg)) {
+  console.log()
+  console.error(
+    `  ${pico.red(' ERROR ')} ${pico.red(`	invalid commit message format.`)}\n\n` +
+      pico.red(
+        `Proper commit message format is required for automated changelog generation. Examples:\n\n`,
+      ) +
+      `    ${pico.green(`feat(compiler): add 'comments' option`)}\n` +
+      `    ${pico.green(`fix: handle events on blur`)}\n\n` +
+      pico.red(`  查看 ./.versionrc 获取基本配置信息.`),
+  )
+  process.exit(1)
+}
