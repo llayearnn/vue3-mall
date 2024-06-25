e71f993cefa0cc106ebb3a4f6ecfdc3d092d802d
王昌 <1255272899@qq.com>
2024-06-25 15:13:41 +0800

    perf: 依赖升级

--- src/views/vxe-demo/index.vue
+++ src/views/vxe-demo/index.vue
@@ -39,21 +39,97 @@
         </template>
       </vxe-column>
     </vxe-table>
+    <el-form mb-50>
+      <el-input
+        v-model="text"
+        style="width: 240px"
+        maxlength="1000"
+        placeholder="Please input"
+        show-word-limit
+        type="text"
+      />
+      <div style="margin: 20px 0" />
+      <el-input
+        v-model="textarea"
+        maxlength="30"
+        style="width: 240px"
+        placeholder="Please input"
+        show-word-limit
+        type="textarea"
+      />
+    </el-form>
+    <el-badge :value="100" :max="1000" class="item">
+      <el-icon size="30px"><Bell /></el-icon>
+    </el-badge>
+    <div>
+      <vxe-grid v-bind="gridOptions"></vxe-grid>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { VxeTableInstance } from 'vxe-table'
+import { VxeTableInstance, VxeGridProps } from 'vxe-table'
+
+const text = ref('')
+const textarea = ref('')
 interface RowVO {
-  id: number
+  id?: number
   name: string
   role: string
   sex: string
   age: number
-  address: string
+  address?: string
   time?: string | Date
+  content?: string
 }
-
+const gridOptions = reactive<VxeGridProps<RowVO>>({
+  rowConfig: {
+    useKey: true,
+  },
+  columnConfig: {
+    useKey: true,
+  },
+  columns: [
+    { type: 'seq', width: 70, title: '序号' },
+    { field: 'name', title: 'Name' },
+    { field: 'sex', title: 'Sex' },
+    { field: 'age', title: 'Age' },
+    { field: 'content', title: 'Html', type: 'html', showOverflow: true },
+    { field: 'role', title: 'Role', showOverflow: true },
+  ],
+  data: [
+    {
+      name: 'Test2',
+      age: 28,
+      sex: '男',
+      role: '后端',
+      content:
+        '<img height="40" src="https://pic2.zhimg.com/50/v2-f7031359103859e1ed38559715ef5f3f_hd.gif">',
+    },
+    {
+      name: 'Test4',
+      age: 26,
+      sex: '男',
+      role: '前端',
+      content: '<a href="https://github.com/x-extends/vxe-table">我是链接</a>',
+    },
+    {
+      name: 'Test3',
+      age: 20,
+      sex: '女',
+      role: '程序员鼓励师',
+      content:
+        '<img height="40" src="https://n.sinaimg.cn/sinacn17/w120h120/20180314/89fc-fyscsmv5911424.gif">',
+    },
+    {
+      name: 'Test1',
+      age: 22,
+      sex: '女',
+      role: '设计师',
+      content: '<div><span style="color: red">我是 Htmp 片段</span></div>',
+    },
+  ],
+})
 const tableData = ref<RowVO[]>([
   {
     id: 10001,
